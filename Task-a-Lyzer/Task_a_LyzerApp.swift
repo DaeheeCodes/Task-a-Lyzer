b727a1ef55feb60ea26431d5ab391657e67889aa
DaeheeCodes <DaeheeCodes@gmail.com>
2022-05-30 11:53:55 -0700

    Initial Commit

---
+++ Task-a-Lyzer/Task_a_LyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_a_LyzerApp.swift
+//  Task-a-Lyzer
+//
+//  Created by DHC on 5/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_a_LyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
